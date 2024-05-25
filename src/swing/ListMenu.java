35d1d3c5741ca714589f25b31e6bad5d9f340f89
pc <pc@DESKTOP-IQIQ6F4>
2024-05-25 07:50:32 +0100

    slight changes in units

--- src/swing/ListMenu.java
+++ src/swing/ListMenu.java
@@ -4,6 +4,7 @@ import model.Model_Menu;
 import java.awt.Component;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.awt.event.MouseMotionAdapter;
 import javax.swing.DefaultListCellRenderer;
 import javax.swing.DefaultListModel;
 import javax.swing.JList;
@@ -14,6 +15,7 @@ public class ListMenu<E extends Object> extends JList<E> {
 
     private final DefaultListModel model;
     private int selectedIndex = -1;
+    private int overIndex = -1;
 
     public ListMenu() {
         model = new DefaultListModel();
@@ -33,6 +35,15 @@ public class ListMenu<E extends Object> extends JList<E> {
                         selectedIndex = index;
                     }
                     repaint();
+                }
+            }
+        });
+        addMouseMotionListener(new MouseMotionAdapter() {
+            @Override
+            public void mouseMoved(MouseEvent e) {
+                int index = locationToIndex(e.getPoint());
+                if (index != overIndex) {
+                    Object o = model.getElementAt(index);
                 }
             }
         });
--- src/swing/MenuItem.java
+++ src/swing/MenuItem.java
@@ -10,6 +10,7 @@ import java.awt.RenderingHints;
 public class MenuItem extends javax.swing.JPanel {
 
     private boolean selected;
+    private boolean over;
 
     public MenuItem(Model_Menu data) {
         initComponents();
@@ -28,6 +29,11 @@ public class MenuItem extends javax.swing.JPanel {
 
     public void setSelected(boolean selected) {
         this.selected = selected;
+        repaint();
+    }
+
+    public void setOver(boolean over) {
+        this.over = over;
         repaint();
     }
 
@@ -63,10 +69,14 @@ public class MenuItem extends javax.swing.JPanel {
 
     @Override
     protected void paintComponent(Graphics grphcs) {
-        if (selected) {
+        if (selected || over) {
             Graphics2D g2 = (Graphics2D) grphcs;
             g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
-            g2.setColor(new Color(255, 255, 255, 80));
+            if (selected) {
+                g2.setColor(new Color(255, 255, 255, 80));
+            } else {
+                g2.setColor(new Color(255, 255, 255, 20));
+            }
             g2.fillRoundRect(10, 0, getWidth() - 20, getHeight(), 5, 5);
         }
         super.paintComponent(grphcs);
